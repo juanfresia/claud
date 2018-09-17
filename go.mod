cab5b24f8e303d98fc04e1fd56f7f5b8aaaf4d18
juanfresia <juanmanuelfresia@gmail.com>
2018-09-16 22:24:36 -0300

    Little endpoint showing message count
    Signed-off-by: juanfresia <juanmanuelfresia@gmail.com>

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/juanfresia/claud
 
 require (
+	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.2 // indirect
 )
--- master/master.go
+++ master/master.go
@@ -2,23 +2,40 @@ package master
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/juanfresia/claud/master/ringo"
+	"github.com/satori/go.uuid"
 )
 
+type Master struct {
+	uuid  uuid.UUID
+	count int
+}
+
+func (m *Master) statusHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, my uuid is %v\n", m.uuid)
+	fmt.Fprintf(w, "I've received %v messages\n", m.count)
+}
+
 func LaunchMaster() {
+	master := &Master{uuid: uuid.NewV4(), count: 0}
+
 	toRingo := make(chan string, 10)
 	fromRingo := make(chan string, 10)
 
 	go ringo.StartRing(toRingo, fromRingo)
+	go receiveFromRingo(fromRingo, master)
 
-	for {
-		// TODO: Expose http endpoints for user here
-		receiveFromRingo(fromRingo)
-	}
+	http.HandleFunc("/", master.statusHandler)
+	http.ListenAndServe(":8081", nil)
 }
 
-func receiveFromRingo(fromRingo <-chan string) {
-	fmt.Println("------------------------------------------------")
-	msg := <-fromRingo
-	fmt.Println("Received message: " + msg)
+func receiveFromRingo(fromRingo <-chan string, master *Master) {
+	for {
+		fmt.Println("------------------------------------------------")
+		msg := <-fromRingo
+		fmt.Println("Received message: " + msg)
+		master.count++
+	}
 }
