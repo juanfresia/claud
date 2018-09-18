42c9fadc6610ad999ad66b80fffe0d10666707b9
AlejandroGarcia95 <alemgarcia95@gmail.com>
2018-09-18 14:10:39 -0300

    Master nodes entries expiring after timeout

--- master/ringo/ringo.go
+++ master/ringo/ringo.go
@@ -12,20 +12,28 @@ const (
 	multicastAddr   = "224.0.0.28:1504"
 	maxDatagramSize = 8192
 	keepAliveTmr    = 5 * time.Second
+	defunctTmr      = 10 * time.Second
+	learningTmr     = 12 * time.Second
 )
 
 // --------------------------- Ringo struct ---------------------------
+
+type masterData struct {
+	uuid  string
+	addr  *net.UDPAddr
+	timer *time.Timer
+}
 
 type ringo struct {
 	uuid       uuid.UUID
 	fromMaster <-chan string
 	toMaster   chan<- string
-	aliveNodes map[string]string
+	aliveNodes map[string]masterData
 }
 
 func newRingo(uuid uuid.UUID, fromMaster <-chan string, toMaster chan<- string) *ringo {
 	r := &ringo{uuid, fromMaster, toMaster, nil}
-	r.aliveNodes = make(map[string]string)
+	r.aliveNodes = make(map[string]masterData)
 	go r.keepAliveSender(multicastAddr)
 	return r
 }
@@ -43,6 +51,21 @@ func (r *ringo) keepAliveSender(multicastAddr string) {
 	}
 }
 
+func (r *ringo) trackMasterNode(masterUuid string, addr *net.UDPAddr) {
+	_, present := r.aliveNodes[masterUuid]
+	if present {
+		r.aliveNodes[masterUuid].timer.Stop()
+	}
+
+	killMasterNode := func() {
+		fmt.Println("Warning: Master " + masterUuid + " has died!")
+		delete(r.aliveNodes, masterUuid)
+	}
+
+	timer := time.AfterFunc(defunctTmr, killMasterNode)
+	r.aliveNodes[masterUuid] = masterData{masterUuid, addr, timer}
+}
+
 func (r *ringo) listenMulticastUDP(multicastAddr string) {
 	addr, err := net.ResolveUDPAddr("udp", multicastAddr)
 	if err != nil {
@@ -57,7 +80,7 @@ func (r *ringo) listenMulticastUDP(multicastAddr string) {
 			fmt.Println("ReadFromUDP failed:", err)
 		}
 		uuidReceived := string(msg[:n])
-		r.aliveNodes[uuidReceived] = src.String()
+		r.trackMasterNode(uuidReceived, src)
 	}
 }
 
@@ -66,7 +89,8 @@ func (r *ringo) handleMasterMessage() {
 	msg := <-r.fromMaster
 	if msg == "MASTERS" {
 		ans := "ALIVE MASTER NODES\n"
-		for uuid, ipPort := range r.aliveNodes {
+		for uuid, md := range r.aliveNodes {
+			ipPort := md.addr.String()
 			ans += ("UUID: " + uuid + " IP:PORT: " + ipPort + "\n")
 		}
 		r.toMaster <- ans
@@ -78,7 +102,8 @@ func (r *ringo) handleMasterMessage() {
 func StartRing(uuid uuid.UUID, fromMaster <-chan string, toMaster chan<- string) {
 	r := newRingo(uuid, fromMaster, toMaster)
 	go r.listenMulticastUDP(multicastAddr)
-	// TODO: Generate ring after some time of listening on multicast
+	time.Sleep(learningTmr)
+	// TODO: Generate ring
 	for {
 		r.handleMasterMessage()
 	}
